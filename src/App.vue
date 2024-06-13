73b81bcb0dbe7c055e8f48f695368214770e969a
Caruso13 <157031605+Caruso13@users.noreply.github.com>
2024-06-13 12:20:24 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+const nome = "Ângelo Caruso"
+const meuObj = {
+  nome: "Ângelo",
+  filmeFavorito: "Rocky"
+}
+
+function dizOla(nome) {
+  return `${nome} diz oi`
+}
+
+
+</script>
+<template>
+  <h1>{{ dizOla() }}</h1>
+</template>
+
+<style scoped>
+
+</style>
