3ee110311495adaee41f10a59a359f2c7ccb1c70
Caruso13 <157031605+Caruso13@users.noreply.github.com>
2024-06-19 15:08:49 -0300

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,34 @@
 <script setup>
-const nome = "Ângelo Caruso"
-const meuObj = {
-  nome: "Ângelo",
-  filmeFavorito: "Rocky"
-}
-
-function dizOla(nome) {
-  return `${nome} diz oi`
-}
-
 
 </script>
+
+
 <template>
-  <h1>{{ dizOla() }}</h1>
+  <div class="container">
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+      <h1>Minhas tarefas</h1>
+      <p>
+        Você possui 7 terefas pendentes
+      </p>
+    </header>
+  </div>
+  <form>
+    <div class="row">
+      <div class="col">
+        <input type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control"> 
+      </div>
+      <div class="col-md-1"></div>
+      <button type="submit" class="btn btn-primary">cadastrar</button> 
+    </div>
+    <div class="col-md-2">
+      <select class="form-control">
+        <option value="todas">Todas tarefas</option>
+        <option value="Pendentes">Pendentes</option>
+        <option value="Finalizadas">Finalizadas</option>
+      </select>
+    </div>
+  </form>
 </template>
 
 <style scoped>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootsrap.min.css';
+
 createApp(App).mount('#app')
